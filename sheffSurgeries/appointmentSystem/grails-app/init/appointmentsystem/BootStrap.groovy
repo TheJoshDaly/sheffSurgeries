3683c0954f807e8b4c78ecaad8382d353e4b08c9
TheJoshDaly <b7034015@my.shu.ac.uk>
2019-04-09 13:20:29 +0100

    Made additional data examples in Bootstrap.groovy

--- sheffSurgeries/appointmentSystem/grails-app/init/appointmentsystem/BootStrap.groovy
+++ sheffSurgeries/appointmentSystem/grails-app/init/appointmentsystem/BootStrap.groovy
@@ -12,6 +12,16 @@ numberOfPatients: 50,
 description: 'Use placeholder text',
 openingTime: '09.00'
     		).save()
+def cityhealth2 = new Surgery (
+name: 'City Health Centre 2: Judgement Day',
+address: '22 Marble Street, Sheffield',
+postcode: 'S10 1NG',
+telephone: '0114666666',
+numberOfPatients: 50,
+description: 'Use placeholder text',
+openingTime: '09.00'
+    		).save()
+
 
 def sarah = new Doctor (
 fullName: 'Dr. Sarah Macdonald',
@@ -20,6 +30,17 @@ position: 'GP, Surgeon',
 doctorEmail: 's.macdonald@myemail.com',
 password: 'secret222',
 doctorOffice: 'D-9888',
+doctorPhone: 01111777,
+bio: 'Use placeholder text'
+    		).save()
+
+def sharkeisha = new Doctor (
+fullName: 'Dr. Sharkeisha Macdonald',
+qualification: 'MBChB(Sheffield)',
+position: 'GP, Surgeon',
+doctorEmail: 'sharky.macdonald@myemail.com',
+password: 'secret223',
+doctorOffice: 'D-9889',
 doctorPhone: 01111777,
 bio: 'Use placeholder text'
     		).save()
@@ -33,12 +54,29 @@ patientID: '46843545',
 dateRegistered: new Date ('20/06/2015'),
 patientPhone: '07791239239').save()
 
+def ketlord = new Patient (
+patientName: 'Lord Kettington',
+patientAddress: '65 Mona Road',
+patientResidence: 'Sheffield',
+patientDob: new Date('10/07/2019'),
+patientID: '46843546',
+dateRegistered: new Date ('21/06/2015'),
+patientPhone: '07791239239').save()
+
 def rob = new Receptionist (
 recepName: 'Rob Kingston',
 recepEmail: 'r.kingston@email.com',
 recepUsername: 'rking',
 recepPassword: 'secret2019',
 recepPhone: 01142224445
+    		).save()
+
+def bob = new Receptionist (
+recepName: 'Bob Ringston',
+recepEmail: 'b.ringston@email.com',
+recepUsername: 'bking',
+recepPassword: 'secret2020',
+recepPhone: 01142224446
     		).save()
 
 def susan = new Nurse ( 
@@ -49,8 +87,23 @@ nurseOffice: 'B-455',
 nursePhone: '01142224433'    		
     		).save()
 
+def peter = new Nurse ( 
+nurseName: 'Peter Susans',
+qualifications: 'Registered General Nurse',
+nurseEmail: 'p.susans@myemail.com',
+nurseOffice: 'B-456',
+nursePhone: '01142224434'    		
+    		).save()
+
 def appntmnts = new Appointment(
 appDate: new Date('11/04/2019'),
+appTime: '15.00',
+appDuration: 30,
+roomNumber: 'A-1111'    		
+    		).save()
+
+def appntmnts2 = new Appointment(
+appDate: new Date('12/04/2019'),
 appTime: '15.00',
 appDuration: 30,
 roomNumber: 'A-1111'    		
@@ -61,6 +114,15 @@ pharmacyName: 'City Centre Pharmacy',
 prescripNumber: 56788,
 medicine: 'Paracetamol',
 totalCost: 5.90,
+dateIssued: new Date('25/05/2019'),
+patientPaying: 'yes'    		
+    		).save() 
+
+def meds2 = new Prescription (
+pharmacyName: 'City Centre Pharmacy',
+prescripNumber: 56789,
+medicine: 'Testosterone Enanthate',
+totalCost: 10.90,
 dateIssued: new Date('25/05/2019'),
 patientPaying: 'yes'    		
     		).save() 
